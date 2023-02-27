74b622d6901d711f4a981ce8d8298273ba357b83
Adel Bagautdinov <abagautdinov1@yandex-team.ru>
2023-02-27 15:07:02 +0400

    Добавил покатушки кнопки

--- IStandardTestApp/Main/View/MainViewController.swift
+++ IStandardTestApp/Main/View/MainViewController.swift
@@ -33,6 +33,9 @@ final class MainViewController: UIViewController {
         return spinner
     }()
 
+    private lazy var bottomButtonConstraint: NSLayoutConstraint = goButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+                                                                                                   constant: -16.0)
+
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter?.viewDidLoad()
@@ -46,6 +49,16 @@ extension MainViewController: MainViewProtocol {
         goButton.addTarget(self, action: #selector(getText), for: .touchUpInside)
         addSubviews()
         makeConstraints()
+
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow(notification:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide(notification:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
     }
 
     func startSpinner() {
@@ -75,9 +88,9 @@ private extension MainViewController {
             textField.heightAnchor.constraint(equalToConstant: 44.0),
 
             goButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            goButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             goButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50.0),
             goButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50.0),
+            bottomButtonConstraint,
             goButton.heightAnchor.constraint(equalToConstant: 44.0),
 
             spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor),
@@ -89,6 +102,35 @@ private extension MainViewController {
 
     @objc func getText() {
         presenter?.tapButton()
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        bottomButtonConstraint.constant = -16
+        view.layoutIfNeeded()
+    }
+
+    @objc func keyboardWillShow(notification : Notification?) -> Void {
+        
+        var _kbSize: CGSize!
+        
+        if let info = notification?.userInfo {
+            
+            let frameEndUserInfoKey = UIResponder.keyboardFrameEndUserInfoKey
+            
+            if let kbFrame = info[frameEndUserInfoKey] as? CGRect {
+                
+                let screenSize = UIScreen.main.bounds
+                let intersectRect = kbFrame.intersection(screenSize)
+                
+                if intersectRect.isNull {
+                    _kbSize = CGSize(width: screenSize.size.width, height: 0)
+                } else {
+                    _kbSize = intersectRect.size
+                }
+                bottomButtonConstraint.constant -= _kbSize.height
+                view.layoutIfNeeded()
+            }
+        }
     }
 }
 
