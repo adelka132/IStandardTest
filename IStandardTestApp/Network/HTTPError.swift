77f69587b01a2c657f2a4d8587ab478ed7c3d787
Adel Bagautdinov <abagautdinov1@yandex-team.ru>
2023-03-01 20:48:25 +0400

    убрал комент

--- IStandardTestApp/Network/HTTPError.swift
+++ IStandardTestApp/Network/HTTPError.swift
@@ -30,9 +30,3 @@ enum HTTPError: Error {
  1) Заменить кастом на дефолт Error
  2) В нужном мне сервисе обрабатываю эту ошбику как мне надо
  */
-
-enum PointServiceError {
-    dskfjhdsjfkhsdfsd
-    sbadasds
-    jklfsdjfklsdf
-}
