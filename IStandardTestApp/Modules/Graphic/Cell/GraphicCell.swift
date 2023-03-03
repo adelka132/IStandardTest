19f5ed3a1f0581290fa1b80023d2104e5298c0e4
Adel Bagautdinov <abagautdinov1@yandex-team.ru>
2023-03-03 10:09:08 +0400

    Поправил модель ячейки

--- IStandardTestApp/Modules/Graphic/Cell/GraphicCell.swift
+++ IStandardTestApp/Modules/Graphic/Cell/GraphicCell.swift
@@ -2,21 +2,9 @@ import UIKit
 
 final class GraphicCell: UITableViewCell {
 
-    private var model: Point? {
-        didSet { makeConfiguration() }
-    }
-
     func set(model: Point) {
-        self.model = model
-    }
-}
-
-private extension GraphicCell {
-    func makeConfiguration() {
         var cellConfig = defaultContentConfiguration()
-        let xPoint = String(optionallyDescribing: model?.x)
-        let yPoint = String(optionallyDescribing: model?.y)
-        cellConfig.text = "x: \(xPoint) y: \(yPoint)"
+        cellConfig.text = "x: \(model.x) y: \(model.y)"
         contentConfiguration = cellConfig
     }
 }
